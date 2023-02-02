941c94701dc044111eb975628f9a66b3bb0e7ab8
SabljicAntonio <102249787+SabljicAntonio@users.noreply.github.com>
2023-02-02 19:03:10 +0100

    Project setup

---
+++ avionlet/src/main/java/com/struczad/avionlet/AvionletApplication.java
@@ -0,0 +1,13 @@
+package com.struczad.avionlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvionletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvionletApplication.class, args);
+	}
+
+}
---
+++ avionlet/src/test/java/com/struczad/avionlet/AvionletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.struczad.avionlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvionletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
